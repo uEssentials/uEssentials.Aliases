87fa06f32624b853063f9341be2030219f0f4a4c
Leonardo Santos <devleeo@outlook.com>
2016-07-25 22:29:24 -0200

    Initial commit

---
+++ src/AliasesModule.cs
@@ -0,0 +1,132 @@
+﻿/*
+ *  This file is part of uEssentials project.
+ *      https://uessentials.github.io/
+ *
+ *  Copyright (C) 2015-2016  Leonardosc
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Essentials.Api;
+using Essentials.Api.Command;
+using Essentials.Api.Command.Source;
+using Essentials.Api.Module;
+using Essentials.Common;
+using Essentials.Common.Util;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Essentials.Modules.Aliases
+{
+    [ModuleInfo(
+        Name = "uEssentials.Aliases",
+        Author = "Leonardosc",
+        Version = "$ASM_VERSION"
+    )]
+    public class AliasesModule : EssModule {
+
+        public List<CommandAlias> RegisteredAliases { get; } = new List<CommandAlias>();
+        private string _aliasesPath;
+
+        public override void OnLoad() {
+            Logger.LogInfo($"Enabling Aliases v{Info.Version}");
+
+            _aliasesPath = Path.Combine(Folder, "aliases.json");
+
+            if (!File.Exists(_aliasesPath)) {
+                CommandAlias[] defaultAliases = {
+                    new CommandAlias("aliastest", "Just a test", null, new [] {"broadcast hi"}, null),
+                    new CommandAlias("aliastest2", "Just a test", null, new [] {"broadcast hi2"}, null),
+                };
+
+                JsonUtil.Serialize(_aliasesPath, defaultAliases);
+            }
+
+            try {
+                LoadAliases(_aliasesPath);
+                RegisterAliases();
+            } catch (Exception cw) {
+                Logger.LogError("An error ocurred while loading aliases.");
+                Logger.LogError("Make sure that you aliases.json are in correct format,");
+                Logger.LogError("then use '/reloadaliases' to try again.");
+                Logger.LogError(cw.ToString());
+            }
+
+            UEssentials.CommandManager.Register(ReloadAliasesCommand);
+        }
+
+        public override void OnUnload() {
+            Logger.LogInfo($"Disabling Aliases v{Info.Version}");
+        }
+
+        [CommandInfo(
+            Name = "reloadaliases",
+            Description = "Reload aliases",
+            Permission = "essentials.aliases.reloadaliases"
+        )]
+        public CommandResult ReloadAliasesCommand(ICommandSource src, ICommandArgs args) {
+            src.SendMessage("Reloading aliases...");
+            var aliasBak = new List<CommandAlias>(RegisteredAliases);
+            RegisteredAliases.Clear();
+
+            try {
+                LoadAliases(_aliasesPath);
+                aliasBak.ForEach(UEssentials.CommandManager.Unregister);
+                RegisterAliases();
+                src.SendMessage("Sucessfully reloaded!");
+            } catch (Exception cw) {
+                RegisteredAliases.AddRange(aliasBak); // Restore aliases
+                return CommandResult.Error("Error found: {0}", cw.Message);
+            }
+            
+            return CommandResult.Success();
+        }
+
+        private void LoadAliases(string path) {
+            var rawAliases = JArray.Parse(File.ReadAllText(path));
+
+            // Load aliases 
+            foreach (var jsonAlias in rawAliases) {
+                var alias = JsonConvert.DeserializeObject<CommandAlias>(jsonAlias.ToString());
+                if (alias.Name == null) {
+                    throw new ArgumentException($"Name of alias is null or invalid. Alias: \n{jsonAlias}");
+                }
+                if (alias.Commands == null || alias.Commands.Length == 0) {
+                    throw new ArgumentException($"Commands of alias '{alias.Name}' is null or empty.");
+                }
+                if (RegisteredAliases.Any(a => a.Name.EqualsIgnoreCase(alias.Name))) {
+                    throw new ArgumentException($"An alias with name '{alias.Name}' is already registered.");
+                }
+                RegisteredAliases.Add(alias);
+            }
+        }
+
+        private void RegisterAliases() {
+            // Register aliases
+            var commandManager = UEssentials.CommandManager;
+
+            RegisteredAliases.ForEach(entry => {
+                commandManager.Register(entry); 
+                #if DEBUG
+                    Logger.LogInfo($"Registered alias: {entry.Name}");
+                #endif
+            });
+        }
+    }
+}
---
+++ src/CommandAlias.cs
@@ -0,0 +1,115 @@
+﻿/*
+ *  This file is part of uEssentials project.
+ *      https://uessentials.github.io/
+ *
+ *  Copyright (C) 2015-2016  Leonardosc
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+using System.Linq;
+using System.Text;
+using Essentials.Api;
+using Essentials.Api.Command;
+using Essentials.Api.Command.Source;
+using Essentials.Common;
+using Newtonsoft.Json;
+
+namespace Essentials.Modules.Aliases {
+
+    public class CommandAlias : ICommand {
+
+        public string Name { get; }
+        public string Usage { get; set; }
+        public string Description { get; set; }
+        public string[] Commands { get; set; }
+        public string[][] ValidInputs { get; set; }
+
+        [JsonIgnore]
+        public string Permission { get; set; }
+
+        [JsonIgnore]
+        public string[] Aliases { get; set; }
+
+        [JsonIgnore]
+        public AllowedSource AllowedSource { get; set; }
+
+        public CommandAlias(string name, string description, string usage, string[] commands, 
+                            string[][] validInputs) {
+            Name = name;
+            Description = description;
+            Usage = usage;
+            Commands = commands;
+            ValidInputs = validInputs;
+            AllowedSource = AllowedSource.BOTH;
+            Aliases = new string[0];
+
+            Permission = $"essentials.alias.{name}";
+        }
+
+        public CommandResult OnExecute(ICommandSource src, ICommandArgs args) {
+            // Validate arguments
+            if (ValidInputs != null && ValidInputs.Where((t, i) => !t.Any(s => s.Equals(args[i].RawValue))).Any()) {
+                return CommandResult.ShowUsage();
+            }
+
+            // Execute commands
+            Commands.Select(c => new StringBuilder(c)).ForEach(command => {
+                command.Replace("$sender", src.DisplayName);
+                StringBuilder allArgBuilder = null;
+
+                if (command.ToString().Contains("$arg*")) {
+                    allArgBuilder = new StringBuilder();    
+                }
+
+                for (var i = 0; i < args.Length; i++) {
+                    var current = args[i];
+                    command.Replace($"$arg{i}", current.RawValue);
+
+                    if (allArgBuilder == null) continue;
+                    if (current.IsString) {
+                        allArgBuilder.Append("\"");
+                        allArgBuilder.Append(current.RawValue);
+                        allArgBuilder.Append("\"");
+                    } else {
+                        allArgBuilder.Append(current.RawValue);
+                    }
+                    if (i != args.Length - 1) {
+                        allArgBuilder.Append(" ");
+                    }
+                }
+
+                if (allArgBuilder != null) {
+                    command.Replace("$arg*", allArgBuilder.ToString());
+                }
+
+                var strCommand = command.ToString();
+
+                if (strCommand.StartsWith("console:")) {
+                    strCommand = strCommand.Remove(0, "console:".Length); // remove 'console:' prefix
+                    UEssentials.ConsoleSource.DispatchCommand(strCommand);
+                } else {
+                    src.DispatchCommand(command.ToString());
+                }
+            });
+            return CommandResult.Success();
+        }
+
+        public override string ToString() {
+            return $"Name: {Name}, Description: {Description}, ValidInputs: {ValidInputs}, Usage: {Usage}, Commands: {Commands}";
+        }
+    }
+
+}
